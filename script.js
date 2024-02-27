0ed16b1d4628cb262d3383aceb1b9a288e6c0979
VSaiBhavana <151862122+VSaiBhavana@users.noreply.github.com>
2024-02-27 20:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ function stringChop(str, size) {
   if (size <= 0) {
         throw new Error("Chunk length must be greater than 0");
     }
-
     const result = [];
     let index = 0;
-
     while (index < str.length) {
-        result.push(str.slice(index, index + size));
-        index += size;
+        const chunk = str.substr(index, chunkLength);
+        result.push(chunk);
+        index += chunkLength;
     }
-
     return result;
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
